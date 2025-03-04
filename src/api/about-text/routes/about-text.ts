39ca0b2d71a474bfa4726c14ec578b937b1fe9b3
fabiodoesmeso <fabio@mesoai.co>
2025-03-04 14:39:32 -0500

    objects

---
+++ src/api/about-text/routes/about-text.ts
@@ -0,0 +1,7 @@
+/**
+ * about-text router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::about-text.about-text');
---
+++ src/api/about-text/services/about-text.ts
@@ -0,0 +1,7 @@
+/**
+ * about-text service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::about-text.about-text');
